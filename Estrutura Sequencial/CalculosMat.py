7323cf5ef888dbb11d5832bf16bd143f675f47ef
JcDev-124 <135718019+JcDev-124@users.noreply.github.com>
2023-06-06 14:05:48 -0300

    Add files via upload
    Algoritmos basicos de estrutura sequencial

---
+++ Estrutura Sequencial/CalculosMat.py
@@ -0,0 +1,11 @@
+numero_inteiro1 = int(input("Digite o primeiro número inteiro: "))
+numero_inteiro2 = int(input("Digite o segundo número inteiro: "))
+numero_real = float(input("Digite um número real: "))
+
+produto = (2 * numero_inteiro1) * (numero_inteiro2 / 2)
+soma = (3 * numero_inteiro1) + numero_real
+cubo = numero_real ** 3
+
+print("O produto do dobro do primeiro com metade do segundo é", produto)
+print("A soma do triplo do primeiro com o terceiro é", soma)
+print("O terceiro elevado ao cubo é", cubo)
---
+++ Estrutura Sequencial/PesoIdeal.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite a altura da pessoa: "))
+
+peso_ideal_homem = (72.7 * altura) - 58
+peso_ideal_mulher = (62.1 * altura) - 44.7
+
+print("O peso ideal para um homem com essa altura é", peso_ideal_homem)
+print("O peso ideal para uma mulher com essa altura é", peso_ideal_mulher)
